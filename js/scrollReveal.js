3e57087981a4abaf3da193381c7adce0c2ee6b81
Nataly Cruz Serrato <natary251610@gmail.com>
2025-06-17 17:58:01 -0500

    se agregaron animaciones

--- js/scrollReveal.js
+++ js/scrollReveal.js
@@ -8,3 +8,14 @@ window.addEventListener('scroll', () => {
     }
   }
 });
+
+window.addEventListener('scroll', () => {
+  const reveals = document.querySelectorAll('.reveal');
+  for (let el of reveals) {
+    const windowHeight = window.innerHeight;
+    const elementTop = el.getBoundingClientRect().top;
+    if (elementTop < windowHeight - 100) {
+      el.classList.add('active');
+    }
+  }
+});
